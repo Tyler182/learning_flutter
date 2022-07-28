9f9c6ef4f48c7828dfac9dd7054b5d2861deb364
Tyler182 <msb7105@gmail.com>
2022-07-28 22:48:28 +0300

    Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,82 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyStateLessWidget());
+void main() => runApp(MyStatefulWidget());
 
-class MyStateLessWidget extends StatelessWidget {
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  State<MyStatefulWidget> createState() {
+    return _MyStatefulWidgetState();
+  }
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool _loading = false;
+  double _progressValue = 0.0;
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          centerTitle:true,
+          centerTitle: true,
           title: Text("Download page"),
         ),
         body: Center(
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(value: 0.4, color: Colors.blue),
-                Text(
-                  "40%",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Text(
-                  "Downloading in progress",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ],
-            ),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(
+                          value: _progressValue, color: Colors.blue),
+                      Text(
+                        "${(_progressValue * 100).round()}%",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      Text(
+                        "Downloading in progress",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  )
+                : Text(
+                    "Press to download",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.cloud),
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _progressValue = 0.0;
+              _updateProgress();
+            });
+          },
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    Duration oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (timer) {
+      setState(() {
+        _progressValue += 0.2;
+      });
+      if (_progressValue >= 1) {
+        setState(() {
+          _loading = false;
+          _progressValue = 0.0;
+        });
+        timer.cancel();
+        return;
+      }
+      ;
+    });
   }
 }
