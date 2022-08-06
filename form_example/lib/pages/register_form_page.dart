088fc34eac4de02ee70d8b30c4a3f07bc565972f
Tyler182 <msb7105@gmail.com>
2022-08-06 22:07:52 +0300

    added styles for form_example

--- form_example/lib/pages/register_form_page.dart
+++ form_example/lib/pages/register_form_page.dart
@@ -20,40 +20,118 @@ class RegisterFormPageState extends State<RegisterFormPage> {
             children: <Widget>[
               const TextField(
                 decoration: InputDecoration(
-                  labelText: 'Full Name',
+                  prefixIcon: Icon(Icons.person),
+                  hintText: 'Surname Name',
+                  helperText: 'Only Surname and Name',
+                  labelText: 'Full Name*',
+                  suffixIcon: Icon(Icons.delete_outline),
+                  suffixIconColor: Colors.red,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
-                  labelText: 'Fphone Number',
+                  prefixIcon: Icon(Icons.phone),
+                  helperText: 'Phone we can call you',
+                  suffixIcon: Icon(Icons.delete_outline),
+                  suffixIconColor: Colors.red,
+                  labelText: 'Phone Number',
+                  hintText: '+x(xxx)xxx-xx-xx',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Email Address',
+                  prefixIcon: Icon(Icons.mail),
+                  hintText: 'Surname Name',
+                  helperText: 'Only Surname and Name',
                 ),
               ),
               const SizedBox(height: 20),
               TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Life Story',
+                  hintText: 'Tell us about yourselve',
+                  helperText: 'Keep it short',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      // color: Colors.black,
+                      // width: 2,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      // width: 2,
+                    ),
+                  ),
                 ),
+                maxLines: 3,
               ),
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
                   labelText: 'Password',
+                  prefixIcon: Icon(Icons.security),
+                  suffixIcon: Icon(Icons.visibility),
+                  hintText: 'Enter password',
+                  helperText: 'Symbols, numbers, special characters',
                 ),
               ),
               const SizedBox(height: 10),
               TextFormField(
                 decoration: const InputDecoration(
-                  labelText: 'Confurm Password',
+                  labelText: 'Confirm Password',
+                  prefixIcon: Icon(Icons.border_color),
+                  hintText: 'Enter passwords',
+                  helperText: 'Same as in field password',
                 ),
               ),
-              const SizedBox(height: 10),              
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green)),
+                onPressed: (() {}),
+                child: const Text('Submit Form'),
+              ),
             ],
           ),
         ));
