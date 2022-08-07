f063c306d92ced1c03a8918a599e938c588cbae7
Tyler182 <msb7105@gmail.com>
2022-08-07 12:40:17 +0300

    added limitations to fields in form_example

--- form_example/lib/pages/register_form_page.dart
+++ form_example/lib/pages/register_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class RegisterFormPage extends StatefulWidget {
   const RegisterFormPage({Key? key}) : super(key: key);
@@ -7,6 +8,7 @@ class RegisterFormPage extends StatefulWidget {
 }
 
 class RegisterFormPageState extends State<RegisterFormPage> {
+  bool _hidePass = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,6 +50,11 @@ class RegisterFormPageState extends State<RegisterFormPage> {
               ),
               const SizedBox(height: 10),
               TextFormField(
+                keyboardType: TextInputType.phone,
+                // inputFormatters: [
+                //   FilteringTextInputFormatter.digitsOnly,
+                // ],
+                maxLength: 16,
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.phone),
                   helperText: 'Phone we can call you',
@@ -77,6 +84,7 @@ class RegisterFormPageState extends State<RegisterFormPage> {
               ),
               const SizedBox(height: 10),
               TextFormField(
+                keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(
                   labelText: 'Email Address',
                   prefixIcon: Icon(Icons.mail),
@@ -92,9 +100,9 @@ class RegisterFormPageState extends State<RegisterFormPage> {
                   helperText: 'Keep it short',
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
-                      // color: Colors.black,
-                      // width: 2,
-                    ),
+                        // color: Colors.black,
+                        // width: 2,
+                        ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
@@ -107,21 +115,43 @@ class RegisterFormPageState extends State<RegisterFormPage> {
               ),
               const SizedBox(height: 10),
               TextFormField(
-                decoration: const InputDecoration(
+                obscureText: _hidePass,
+                maxLength: 12,
+                decoration: InputDecoration(
                   labelText: 'Password',
                   prefixIcon: Icon(Icons.security),
-                  suffixIcon: Icon(Icons.visibility),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _hidePass = !_hidePass;
+                      });
+                    },
+                    icon: _hidePass
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off),
+                  ),
                   hintText: 'Enter password',
                   helperText: 'Symbols, numbers, special characters',
                 ),
               ),
               const SizedBox(height: 10),
               TextFormField(
-                decoration: const InputDecoration(
+                obscureText: _hidePass,
+                decoration: InputDecoration(
                   labelText: 'Confirm Password',
                   prefixIcon: Icon(Icons.border_color),
                   hintText: 'Enter passwords',
                   helperText: 'Same as in field password',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _hidePass = !_hidePass;
+                      });
+                    },
+                    icon: _hidePass
+                      ? Icon(Icons.visibility)
+                      : Icon(Icons.visibility_off),
+                  ),
                 ),
               ),
               const SizedBox(height: 10),
